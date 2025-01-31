af20478112d26542960a16111be4b55529c74405
Yiğit <40565628+tryigit@users.noreply.github.com>
2025-02-01 02:30:56 +0300

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,16 @@
+# tryigitx
+
+ui_print " "
+ui_print " Version: $MODVER"
+ui_print " Telegram: t.me/cleverestech"
+ui_print " "
+ui_print "- Android version check..."
+# Error on < Android 11.
+if [ "$API" -lt 28 ]; then
+    ui_print " "
+    ui_print "❗ You can't use this module on Android < 9"
+    abort " "
+fi
+ui_print " "
+ui_print " "
+ui_print " - Done, please reboot system"
