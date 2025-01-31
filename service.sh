4daa079e7fe25be48efe0e59a5615a8d8f98e60a
Yiğit <40565628+tryigit@users.noreply.github.com>
2025-02-01 02:32:19 +0300

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,11 @@
+# USAP Settings @tryigitx
+resetprop persist.sys.usap_pool_enabled false
+resetprop persist.sys.dynamic_usap_enabled false
+resetprop persist.sys.prestart.proc false
+resetprop persist.sys.prestart.feedback.enable=false
+resetprop persist.sys.launch_response_optimization.enable false
+
+# resetprop debug.gr.swapinterval 0
+
+# Gpu Optimization @tryigitx
+resetprop debug.sf.enable_gl_backpressure 1
