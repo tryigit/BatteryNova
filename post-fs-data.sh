b97967dde6c78440b1edc6c96ce38cf9e4192e03
Yiğit <40565628+tryigit@users.noreply.github.com>
2025-02-01 14:33:44 +0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,9 +5,9 @@ resetprop persist.sys.prestart.proc false
 resetprop persist.sys.prestart.feedback.enable false
 resetprop persist.sys.launch_response_optimization.enable false
 resetprop persist.vendor.sys.activity_resumption_boost false
-resetprop persist.sys.gpu.early_app_start false
 
 # resetprop debug.gr.swapinterval 0
 
 # Gpu Optimization @tryigitx
 resetprop debug.sf.enable_gl_backpressure 1
+resetprop persist.sys.gpu.early_app_start false
