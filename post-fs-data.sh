67179a244d2f1266abece9ff15555d6f0beb033e
Yiğit <40565628+tryigit@users.noreply.github.com>
2025-02-01 14:33:10 +0300

    Update and rename service.sh to post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,6 +4,8 @@ resetprop persist.sys.dynamic_usap_enabled false
 resetprop persist.sys.prestart.proc false
 resetprop persist.sys.prestart.feedback.enable false
 resetprop persist.sys.launch_response_optimization.enable false
+resetprop persist.vendor.sys.activity_resumption_boost false
+resetprop persist.sys.gpu.early_app_start false
 
 # resetprop debug.gr.swapinterval 0
 
